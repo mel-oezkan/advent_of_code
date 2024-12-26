533e8b36aaef8e8899de5936eea92cff2fd188d7
mel-oezkan <meloezkan@uni-osnabrueck.de>
2024-12-26 16:53:01 +0100

    building blocks 08

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 The following repo contains the solutions for the Advent of code 2024. This repo was created to only solve the basic problems of the current advent of code using a new programming language namely Rust.
 
 Solutions are not optimal and are only supposed to make myself more familiar with Rust data structures and code flow.
+
+## Solution Strategy
+
+### Day 08
+
+-   For any signal find all the possible positions
+    -   O(n): Iterate over the matrix and get unique symbol types (#k) and position
+-   Using the signal positions iteratively generate the new antinodes
+    -   O(#k\*#n): crete the positions
--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -1,12 +1,29 @@
+use std::collections::HashMap;
 
+
+
+type Position = (i32, i32);
 
 struct Solution {
-
+    board: Vec<Vec<char>>,
+    antenna_table: HashMap<char, Vec<Position>>,
 }
 
 impl Solution {
-    pub fn new() -> Self {
-        Solution {}
+    pub fn new(path: String) -> Self {
+
+        let board: Vec<Vec<char>> = std::fs::read_to_string(path)
+            .expect("Failed to read file")
+            .lines()
+            .map(|line| line.chars().collect())
+            .collect();
+
+        let mut antenna_table: HashMap<char, Vec<Position>> = HashMap::new();
+
+        Solution {
+            board,
+            antenna_table
+        }
     }
 }
 
@@ -14,6 +31,6 @@ fn main() {
     // let path = String::from("inputs/day08.txt");
     let path = String::from("test_inputs/day08.txt");
 
-    let mut solution = Solution::new();
+    let mut solution = Solution::new(path);
 }
 
